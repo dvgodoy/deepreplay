b75318e5890ff86c7d46f1b41bd55d7126832082
dvgodoy <dvgodoy@gmail.com>
2018-04-20 22:19:50 +0200

    docstrings for dataset

--- deepreplay/datasets/parabola.py
+++ deepreplay/datasets/parabola.py
@@ -1,6 +1,30 @@
 import numpy as np
 
 def load_data(xlim=(-1, 1), n_points=1000, shuffle=True, seed=13):
+    """Generates a dataset composed of two parabolas, with `n_points`
+    each. The upper parabola represents the negative cases (class 0)
+    and the lower parabola, shifted 0.5 from the first, represents
+    the positive cases (class 1).
+
+    Parameters
+    ----------
+    xlim: tuple of ints, optional
+        Boundaries for the X axis. Default is (-1, 1)
+    n_points: int, optional
+        Number of points in each parabola. Default is 1,000.
+    shuffle: boolean, optional
+        If True, the points are shuffled. Default is True.
+    seed: int, optional
+        Random seed. Default is 13.
+
+    Returns
+    -------
+    X, y: tuple of ndarray
+        X is an array of shape (2 * n_points, 2) containing the
+        samples for the two parabolas.
+        y is an array of shape (2 * n_points, 1) containing the
+        classes of the samples.
+    """
     # feature x1, 1,000 points evenly spaced between -1 and 1
     x1 = np.linspace(xlim[0], xlim[1], n_points)
     # feature x2, for the two curves
